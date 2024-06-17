57ca3dcffc2538e40b6ff489ba12c1ce02d3a91f
Eunchan Noh <testora_@naver.com>
2024-06-17 09:59:39 +0900

    bt

---
+++ Assets/Scripts/Player/BulletController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BulletController : MonoBehaviour
+{
+    PlayerController player;
+    public Vector3 vector = Vector3.zero;
+
+    [SerializeField] float speed = 100f;
+    [SerializeField] float lifeTime = 1f;
+
+    int damage = 1;
+    public int Damage { get { return damage; } }
+
+    void Awake()
+    {
+        //playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += vector * speed * Time.deltaTime;
+    }
+
+    void OnEnable()
+    {
+        if (IsInvoking("RetrieveBullet"))
+            CancelInvoke("RetrieveBullet");
+        Invoke("RetrieveBullet", lifeTime);
+    }
+
+    #region Public Methods
+
+    public void InitBullet(PlayerController controller)
+    {
+        player = controller;
+    }
+
+    public void RetrieveBullet()
+    {
+        if (IsInvoking("RetrieveBullet"))
+            CancelInvoke("RetrieveBullet");
+        player.RetrieveBullet(this);
+    }
+
+    #endregion
+}
